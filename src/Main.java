49411ee1ffb853188d5ef45d82f243c7c2746cba
BananaFlamber <KorolevSteel@inbox.ru>
2022-12-18 20:40:53 +0300

    All task - done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,12 +15,16 @@ public class Main {
 
         System.out.println("Сгенерированый список -> " + list);
 
-        delOdd(list);
+        findMaxValue(list);
+        findMinValue(list);
+        findAverage(list);
 
-        System.out.println("Список после удаления четных чисел -> " + list);
+//        delOdd(list);
+
+//        System.out.println("Список после удаления четных чисел -> " + list);
     }
 
-    public static ArrayList<Integer> delOdd(ArrayList<Integer> array) {
+    public static ArrayList<Integer> delOdd(ArrayList<Integer> array) {   // Функция удаления элементов ссписка с четными числами
         for (int i = 0; i < array.size(); i++) {
             if (array.get(i) % 2 == 0) {
                 array.remove(i);
@@ -29,5 +32,35 @@ public class Main {
             }
         }
         return array;
+    }
+
+    public static void findMaxValue(ArrayList<Integer> array) {     // Функция нахождения максимального значения
+        int max = array.get(0);
+        for (int i: array) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        System.out.println( "Максимальный элемент списка со значением ->" + max);
+    }
+
+    public static void findMinValue(ArrayList<Integer> array) {     // Функция нахождения минимального значения
+        int min = array.get(0);
+        for (int i: array) {
+            if (i < min) {
+                min = i;
+            }
+        }
+        System.out.println("Минимальный элемент списка со значением ->" + min);
+    }
+
+    public static void findAverage(ArrayList<Integer> array) {    // Функция нахождения среднего арифметического
+//        OptionalInt average = Arrays.stream(array).average(); полчему-то таким способом не получается решить, идея ругается.
+        int average = 0;
+        for(int i: array) {
+            average = average + i;
+        }
+        double result = average / array.size();
+        System.out.println( "Среднее арифметическое значение элементов списка -> " + result);
     }
 }
